645e34529e4dea4dd160c118de694c1daf607266
신재혁 <tlswogur0603@gmail.com>
2026-03-15 19:25:55 +0900

    feat: 주식(Stock) 테이블 데이터 모델 추가

---
+++ backend/app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Float
+from .database import Base
+
+class Stock(Base):
+    __tablename__ = "stocks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    symbol = Column(String, index=True)
+    name = Column(String)
+    price = Column(Float)
