d2e94e3666af0130a1715b91a4ba68771e9564fe
신재혁 <tlswogur0603@gmail.com>
2026-03-17 00:13:12 +0900

    feat: 주식 목록 조회(GET) API 구현 및 코드 주석 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,11 +3,14 @@ from sqlalchemy import text
 from sqlalchemy.orm import Session
 from .database import engine, SessionLocal
 from . import models, schemas
+from typing import List
 
+# 데이터베이스 테이블 생성
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# 데이터베이스 세션 연결 및 종료 관리
 def get_db():
     db = SessionLocal()
     try:
@@ -15,6 +18,13 @@ def get_db():
     finally:
         db.close()
 
+# 전체 주식 목록 조회
+@app.get("/stocks", response_model=List[schemas.StockRead])
+def read_stocks(db: Session = Depends(get_db)):
+    stocks = db.query(models.Stock).all()
+    return stocks
+
+# 신규 주식 정보 저장
 @app.post("/stocks")
 def create_stock(stock: schemas.StockCreate, db: Session = Depends(get_db)):
     db_stock = models.Stock(
@@ -31,10 +41,13 @@ def create_stock(stock: schemas.StockCreate, db: Session = Depends(get_db)):
 
     return{"message": "저장 완료!", "data": db_stock}
 
+
+# 서버 테스트
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
 
+# DB연결 테스트
 @app.get("/db-test")
 def test_db():
     try:
